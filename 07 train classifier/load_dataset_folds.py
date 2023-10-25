738f7af28654da57a2a79c3ea7df266d6468eca5
hen58277 <nils-constantin.hellwig@stud.uni-regensburg.de>
2023-10-25 10:50:38 +0200

    optimise language diversity analysis

--- 07 train classifier/load_dataset_folds.py
+++ 07 train classifier/load_dataset_folds.py
@@ -33,7 +33,7 @@ def load_dataset_folds(LLM_NAME, N_REAL, N_SYNTH, LLM_SAMPLING):
     # 2. Load Test Data
     test_dataset = []
     for i in range(constants.N_FOLDS):
-        with open(f'real/split_{i}.json', 'r') as json_datei:
+        with open(f'../07 train classifier/real/split_{i}.json', 'r') as json_datei:
             real_split = json.load(json_datei)
             random.shuffle(real_split)
             test_dataset.append(real_split)
@@ -44,7 +44,7 @@ def load_dataset_folds(LLM_NAME, N_REAL, N_SYNTH, LLM_SAMPLING):
     # 3.1 Add 10 real samples if condition is fixed examples
     if LLM_SAMPLING == "fixed":
         for split_idx in constants.SPLIT_LOOP[1:constants.N_FOLDS+1]:
-            with open(f'real_fixed/split_{split_idx}.json', 'r') as json_datei:
+            with open(f'../07 train classifier/real_fixed/split_{split_idx}.json', 'r') as json_datei:
                 real_split = json.load(json_datei)
                 random.shuffle(real_split)
                 train_dataset.append(real_split)
@@ -57,7 +57,7 @@ def load_dataset_folds(LLM_NAME, N_REAL, N_SYNTH, LLM_SAMPLING):
             real_data_splits = constants.SPLIT_LOOP[idx_real +
                                                     1: idx_real+n_splits_required_real+1]
             for split in real_data_splits:
-                with open(f'real/split_{split}.json', 'r') as json_datei:
+                with open(f'../07 train classifier/real/split_{split}.json', 'r') as json_datei:
                     real_split = json.load(json_datei)
                 for example in real_split:
                     train_dataset[idx_real].append(example)
@@ -66,7 +66,7 @@ def load_dataset_folds(LLM_NAME, N_REAL, N_SYNTH, LLM_SAMPLING):
     # 4. Load Synth Train Data
     if N_SYNTH > 0:
         for split_id in range(constants.N_FOLDS):
-            with open(f'synth/{LLM_NAME}/{LLM_SAMPLING}/split_{constants.SPLIT_LOOP[split_id+1]}.json', 'r') as json_datei:
+            with open(f'../07 train classifier/synth/{LLM_NAME}/{LLM_SAMPLING}/split_{constants.SPLIT_LOOP[split_id+1]}.json', 'r') as json_datei:
                 synth_split = json.load(json_datei)[:N_SYNTH]
                 for example in synth_split:
                     train_dataset[split_id].append(example)
