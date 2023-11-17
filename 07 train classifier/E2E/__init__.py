9a88c7e8fc12c29919c251d533ec8379f690d2f7
hen58277 <nils-constantin.hellwig@stud.uni-regensburg.de>
2023-11-17 20:39:28 +0100

    add evaluation script

--- 07 train classifier/E2E/__init__.py
+++ 07 train classifier/E2E/__init__.py
@@ -1,6 +1,8 @@
 from E2E.preprocessing import get_preprocessed_data_E2E
+from helper import format_seconds_to_time_string
 from E2E.model import get_trainer_E2E
 from transformers import AutoTokenizer
+import numpy as np
 import constants
 import time
 
@@ -14,11 +16,13 @@ def train_E2E_model(LLM_NAME, N_REAL, N_SYNTH, TARGET, LLM_SAMPLING, train_datas
         "LLM_SAMPLING": LLM_SAMPLING,
     }
 
-    metrics_classwise_prefix = ["accuracy", "precision", "recall", "f1"]
-    metrics_total_prefix = ["f1_micro", "f1_macro",
-                            "precision", "recall", "accuracy"]
-    metrics = {f"{metric}": [] for metric in metrics_total_prefix}
-    metrics.update({f"{metric}_{ac}": [] for metric in metrics_classwise_prefix for ac in constants.POLARITIES})
+    metrics_classwise_prefix = [
+        "accuracy", "precision", "recall", "f1", "tp", "tn", "fp", "fn"]
+    metrics_total_prefix = ["f1_micro", "f1_macro", "precision",
+                            "recall", "accuracy", "f1", "tp", "tn", "fp", "fn"]
+    metrics_total = {f"{metric}": [] for metric in metrics_total_prefix}
+    metrics_total.update({f"{metric}_{ac}": []
+                         for metric in metrics_classwise_prefix for ac in constants.POLARITIES})
 
     eval_loss = []
     n_samples_train = []
@@ -33,7 +37,37 @@ def train_E2E_model(LLM_NAME, N_REAL, N_SYNTH, TARGET, LLM_SAMPLING, train_datas
         train_data = train_dataset[cross_idx]
         test_data = test_dataset[cross_idx]
 
-        train_data, test_data = get_preprocessed_data_E2E(train_data, test_data, tokenizer)
+        train_data, test_data = get_preprocessed_data_E2E(
+            train_data, test_data, tokenizer)
         trainer = get_trainer_E2E(train_data, test_data, tokenizer)
 
         trainer.train()
+
+        eval_metrics = trainer.evaluate()
+        print(f"Split {cross_idx}:", eval_metrics)
+        eval_loss.append(eval_metrics["eval_loss"])
+        n_samples_train.append(len(train_data))
+        n_samples_test.append(len(test_data))
+
+        for m in metrics_total_prefix:
+            metrics_total[m].append(eval_metrics[f"eval_{m}"])
+
+        for polarity in constants.POLARITIES:
+            for classwise_metric in metrics_classwise_prefix:
+                metrics_total[f"{classwise_metric}_{polarity}"].append(
+                    eval_metrics[f"eval_{classwise_metric}_{polarity}"])
+
+    runtime = time.time() - start_time
+
+    results.update({f"eval_{m}": np.mean(metrics_total[f"{m}"]) for m in metrics_total_prefix})
+    results.update({f"eval_{m}_{polarity}": np.mean(metrics_total[f"{m}_{polarity}"]) for m in metrics_classwise_prefix for polarity in constants.POLARITIES})
+
+    results["runtime"] = runtime
+    results["runtime_formatted"] = format_seconds_to_time_string(runtime)
+    results["eval_loss"] = np.mean(eval_loss)
+    results["n_samples_train"] = n_samples_train
+    results["n_samples_train_mean"] = np.mean(n_samples_train)
+    results["n_samples_test"] = n_samples_test
+    results["n_samples_test_mean"] = np.mean(n_samples_test)
+
+    return results
--- 07 train classifier/E2E/evaluation.py
+++ 07 train classifier/E2E/evaluation.py
@@ -1,9 +1,11 @@
+from helper import divide
 import numpy as np
 import constants
 
-def one_hot_to_label(one_hot):
-    return next(constants.ID_TO_LABEL_E2E[idx] for idx in range(len(one_hot)) if one_hot[idx] == 1)
 
+def add_end_to_tag(tag, i):
+    tag["end"] = i
+    return tag
 
 
 def compute_metrics_for_subset(examples_predictions, examples_labels):
@@ -11,21 +13,110 @@ def compute_metrics_for_subset(examples_predictions, examples_labels):
     tn = 0
     fp = 0
     fn = 0
-    print(examples_labels[0])
-    raise ValueError("This is a custom error message.")
+    for i in range(len(examples_predictions)):
+        example_metrics = calculate_tp_tn_fp_fn_spans(
+            examples_predictions[i], examples_labels[i])
+        tp += example_metrics[0]
+        tn += example_metrics[1]
+        fp += example_metrics[2]
+        fn += example_metrics[3]
+    return tp, tn, fp, fn
+
+
+def calculate_tp_tn_fp_fn_spans(pred, label):
+    pred_set = set(
+        f"{range['start']}_{range['end']}_{range['polarity']}" for range in pred)
+    label_set = set(
+        f"{range['start']}_{range['end']}_{range['polarity']}" for range in label)
+
+    # Calculate true positives by finding the intersection of the sets.
+    tp_set = pred_set & label_set
+    tp = len(tp_set)
+
+    # Calculate false positives by subtracting the intersection from the predicted set.
+    fp_set = pred_set - tp_set
+    fp = len(fp_set)
+
+    # Calculate false negatives by subtracting the intersection from the label set.
+    fn_set = label_set - tp_set
+    fn = len(fn_set)
+
+    return tp, 0, fp, fn
+
+
+def get_predicted_phrases(example):
+    total_tags = []
+    for polarity in constants.POLARITIES:
+        tags_for_polarity = []
+
+        for i in range(len(example)):
+            I_tag_val = example[i][constants.LABEL_TO_ID_E2E[f"I_{polarity}"]]
+            if I_tag_val == 0 and len(tags_for_polarity) > 0:
+                tags_for_polarity = [add_end_to_tag(
+                    tag, i) for tag in tags_for_polarity]
+                total_tags += tags_for_polarity
+                tags_for_polarity = []
+
+            B_tag_val = example[i][constants.LABEL_TO_ID_E2E[f"B_{polarity}"]]
+            if B_tag_val == 1:
+                tags_for_polarity.append({"start": i, "polarity": polarity})
+
+    # print([d for d in total_tags if 'end' not in d])
+    return total_tags
+
+
+def calculate_popular_metrics(predictions, true_labels):
+    tp, tn, fp, fn = compute_metrics_for_subset(predictions, true_labels)
+
+    metrics = {}
+
+    metrics["tp"] = tp
+    metrics["tn"] = tn
+    metrics["fp"] = fp
+    metrics["fn"] = fn
+    metrics["accuracy"] = divide((tp+tn), (tp+tn+fp+fn))
+    metrics["precision"] = divide(tp, tp + fp)
+    metrics["recall"] = divide(tp, tp + fn)
+    metrics["f1"] = divide(2 * metrics["precision"] *
+                           metrics["recall"], metrics["precision"] + metrics["recall"])
+    return metrics
+
+
+def calculate_f1_micro(metrics):
+    tp_total = sum([metrics[f"tp_{pol}"] for pol in constants.POLARITIES])
+    fp_total = sum([metrics[f"fp_{pol}"] for pol in constants.POLARITIES])
+    fn_total = sum([metrics[f"fn_{pol}"] for pol in constants.POLARITIES])
+    tn_total = sum([metrics[f"tn_{pol}"] for pol in constants.POLARITIES])
+
+    precision_total = divide(tp_total, (tp_total + fp_total))
+    recall_total = divide(tp_total, (tp_total + fn_total))
+
+    return divide(2 * (precision_total * recall_total), (precision_total + recall_total))
 
 
 def compute_metrics_E2E(test_data):
 
     def compute_metrics(p):
         predictions, true_labels = p
-        np.save('test_data.npy', test_data)
-        np.save('predictions.npy', predictions)
-        np.save('true_labels.npy', true_labels)
+        # Convert prediction to phrases
+        predictions = [get_predicted_phrases(
+            example) for example in predictions]
+        true_labels = [get_predicted_phrases(
+            example) for example in true_labels]
 
         metrics = {}
+        # Calculate metrics for each polarity
+        for polarity in constants.POLARITIES:
+            pol_metrics = calculate_popular_metrics([[tag for tag in pred if tag["polarity"] == polarity] for pred in predictions], [
+                                                    [tag for tag in label if tag["polarity"] == polarity] for label in true_labels])
+            metrics.update(
+                {f"{metric}_{polarity}": pol_metrics[metric] for metric in pol_metrics.keys()})
 
-        tp, tn, fp, fn = compute_metrics_for_subset(predictions, true_labels)
+        # Total metrics
+        metrics["f1_micro"] = calculate_f1_micro(metrics)
+        metrics["f1_macro"] = sum(metrics[key] for key in [
+                                  f"f1_{pol}" for pol in constants.POLARITIES]) / len(constants.POLARITIES)
+        metrics.update(calculate_popular_metrics(predictions, true_labels))
 
         return metrics
 
