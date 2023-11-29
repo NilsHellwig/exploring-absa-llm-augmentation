67c486dbbf3bebcb378c5a469381037f0744ae6f
hen58277 <nils-constantin.hellwig@stud.uni-regensburg.de>
2023-11-29 19:32:52 +0100

    store log history

--- 07 train classifier/ACD/__init__.py
+++ 07 train classifier/ACD/__init__.py
@@ -21,6 +21,8 @@ def train_ACD_model(LLM_NAME, N_REAL, N_SYNTH, TARGET, LLM_SAMPLING, train_datas
     n_samples_train = []
     n_samples_test = []
     n_samples_validation = []
+    n_epochs_best_validation_score = []
+    log_history = {}
 
     start_time = time.time()
 
@@ -43,6 +45,10 @@ def train_ACD_model(LLM_NAME, N_REAL, N_SYNTH, TARGET, LLM_SAMPLING, train_datas
         # Train Model
         trainer = get_trainer_ACD(train_data, validation_data, tokenizer, results, cross_idx)
         trainer.train()
+
+        # Get index of best epoch on validation data / save log history
+        n_epochs_best_validation_score.append(trainer.state.epoch - constants.N_EPOCHS_EARLY_STOPPING_PATIENCE)
+        log_history[cross_idx] = trainer.state.log_history
 
         # Save Evaluation of Test Data
         eval_metrics = trainer.evaluate(test_data)
@@ -70,5 +76,8 @@ def train_ACD_model(LLM_NAME, N_REAL, N_SYNTH, TARGET, LLM_SAMPLING, train_datas
     results["n_samples_test_mean"] = np.mean(n_samples_test)
     results["n_samples_validation"] = n_samples_validation
     results["n_samples_validation_mean"] = np.mean(n_samples_validation)
+    results["n_epochs_best_validation_score"] = n_epochs_best_validation_score
+    results["n_epochs_best_validation_score_mean"] = np.mean(n_epochs_best_validation_score)
+    results["log_history"] = log_history
 
     return results
