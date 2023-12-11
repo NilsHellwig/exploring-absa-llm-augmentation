a808c0963f3a666f26b5b185ffc8d38855a87876
hen58277 <nils-constantin.hellwig@stud.uni-regensburg.de>
2023-12-11 10:38:46 +0100

    correct path and add conda

--- 07 train models/load_dataset_folds.py
+++ 07 train models/load_dataset_folds.py
@@ -46,7 +46,7 @@ def load_dataset_folds(LLM_NAME, N_REAL, N_SYNTH, LLM_SAMPLING):
     # 2. Load Test Data
     test_dataset = []
     for i in range(1, constants.N_FOLDS+1):
-        with open(f'../07 train classifier/real/split_{constants.SPLIT_LOOP[i]}.json', 'r') as json_datei:
+        with open(f'../07 train models/real/split_{constants.SPLIT_LOOP[i]}.json', 'r') as json_datei:
             real_split = json.load(json_datei)
             random.shuffle(real_split)
             test_dataset.append(real_split)
@@ -54,7 +54,7 @@ def load_dataset_folds(LLM_NAME, N_REAL, N_SYNTH, LLM_SAMPLING):
     # 2. Load Test Data
     validation_dataset = []
     for i in range(2, constants.N_FOLDS+2):
-        with open(f'../07 train classifier/real/split_{constants.SPLIT_LOOP[i]}.json', 'r') as json_datei:
+        with open(f'../07 train models/real/split_{constants.SPLIT_LOOP[i]}.json', 'r') as json_datei:
             real_split = json.load(json_datei)
             random.shuffle(real_split)
             validation_dataset.append(real_split)
@@ -65,7 +65,7 @@ def load_dataset_folds(LLM_NAME, N_REAL, N_SYNTH, LLM_SAMPLING):
     # 3.1 Add 25 real samples if condition is fixed examples
     if LLM_SAMPLING == "fixed" and N_REAL == 25:
         for split_idx in constants.SPLIT_LOOP[0:constants.N_FOLDS]:
-            with open(f'../07 train classifier/real_fixed/split_{split_idx}.json', 'r') as json_datei:
+            with open(f'../07 train models/real_fixed/split_{split_idx}.json', 'r') as json_datei:
                 real_split = json.load(json_datei)
                 random.shuffle(real_split)
                 train_dataset.append(real_split)
@@ -82,7 +82,7 @@ def load_dataset_folds(LLM_NAME, N_REAL, N_SYNTH, LLM_SAMPLING):
         # load first split
         for idx_real in range(constants.N_FOLDS):
             train_dataset.append([])
-            with open(f'../07 train classifier/real/split_{idx_real}.json', 'r') as json_datei:
+            with open(f'../07 train models/real/split_{idx_real}.json', 'r') as json_datei:
                 real_split = json.load(json_datei)
             for example in real_split:
                 train_dataset[idx_real].append(example)
@@ -94,7 +94,7 @@ def load_dataset_folds(LLM_NAME, N_REAL, N_SYNTH, LLM_SAMPLING):
                 real_data_splits = constants.SPLIT_LOOP[2 +
                                                         idx_real: 2 + idx_real + n_splits_required_real - 1]
                 for split in real_data_splits:
-                    with open(f'../07 train classifier/real/split_{split}.json', 'r') as json_datei:
+                    with open(f'../07 train models/real/split_{split}.json', 'r') as json_datei:
                         real_split = json.load(json_datei)
                     for example in real_split:
                         train_dataset[idx_real].append(example)
@@ -103,7 +103,7 @@ def load_dataset_folds(LLM_NAME, N_REAL, N_SYNTH, LLM_SAMPLING):
     # 4. Load Synth Train Data
     if N_SYNTH > 0:
         for split_id in range(constants.N_FOLDS):
-            with open(f'../07 train classifier/synth/{LLM_NAME}/{LLM_SAMPLING}/split_{constants.SPLIT_LOOP[split_id]}.json', 'r') as json_datei:
+            with open(f'../07 train models/synth/{LLM_NAME}/{LLM_SAMPLING}/split_{constants.SPLIT_LOOP[split_id]}.json', 'r') as json_datei:
                 synth_split = json.load(json_datei)[:N_SYNTH]
                 for example in synth_split:
                     train_dataset[split_id].append(example)
