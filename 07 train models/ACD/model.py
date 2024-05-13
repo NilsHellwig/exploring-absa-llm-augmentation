c0ca07dd03abee580192a8c57664415ff5d845a2
hen58277 <nils-constantin.hellwig@stud.uni-regensburg.de>
2024-05-13 10:29:43 +0200

    add hyperparameter getter

--- 07 train models/ACD/model.py
+++ 07 train models/ACD/model.py
@@ -18,10 +18,10 @@ def get_trainer_ACD(train_data, test_data, tokenizer, results, cross_idx, hyperp
         output_dir=constants.OUTPUT_DIR_ACD+"_" +
         results["LLM_NAME"]+"_"+str(results["N_REAL"])+"_"+str(results["N_SYNTH"]) +
         "_"+results["TARGET"]+"_"+results["LLM_SAMPLING"]+"_"+str(cross_idx),
-        learning_rate=constants.LEARNING_RATE_ACD,
-        num_train_epochs=constants.EPOCHS_ACD,
-        per_device_train_batch_size=constants.BATCH_SIZE_ACD,
-        per_device_eval_batch_size=constants.BATCH_SIZE_ACD,
+        learning_rate=hyperparameters["learning_rate"],
+        num_train_epochs=hyperparameters["n_epochs"],
+        per_device_train_batch_size=hyperparameters["batch_size"],
+        per_device_eval_batch_size=hyperparameters["batch_size"],
         save_strategy="epoch" if constants.EVALUATE_AFTER_EPOCH == True else "no",
         logging_dir="logs",
         logging_steps=100,
